6ac87ae17cffe621c58085002a87c14b72deeb2f
marek777-lol <72042275+marek777-lol@users.noreply.github.com>
2022-02-04 09:18:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+document.querySelector('.img2').animate([
+    // keyframes
+    {opacity: '1'},
+    {opacity: '0'}
+  ], {
+    // timing options
+    duration: 10000,
+    iterations: Infinity
+  });
